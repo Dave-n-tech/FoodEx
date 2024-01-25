9509a98d6a26da451a4fd108df44f16559b105c5
Miftau <miftaudeenb@gmail.com>
2024-01-25 17:10:00 +0100

    getting things ready

--- back_end/server.js
+++ back_end/server.js
@@ -1,18 +1,49 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
+const itemRoutes = require('./routes/item');
+const cartRoutes = require('./routes/cart');
+const orderRoutes = require('./routes/order');
+const helmet = require('helmet');
+require('dotenv').config();
 
-const app = express()
-const mongoose = require('mongoose')
-const port = 3000
+const app = express();
 
-app.listen(port, ()=> {
-    console.log(`Server running on port ${port}`)
-})
+app.use(helmet());
+app.use(express.json());
+app.use(cors());
+app.use('/api', authRoutes);
+app.use('/api', itemRoutes);
+app.use('/api', cartRoutes);
+app.use('/api', orderRoutes);
 
-// middleware for getting for data
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
 
-app.use(express.urlencoded({extended: true}))
+/*production environment
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
+*/
 
-// databse connection
-mongoose.connect('mongodb://127.0.0.1:27017/progress-DB')
-.then(() => console.log('Database connected successfully'))
-.catch(err => console.log(err.message))
+//MongoDB connection
+const PORT = process.env.PORT || 4000;
+const dbURI = process.env.DB_URI;
+
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(result =>
+    app.listen(PORT, () => {
+      console.log(`Server running on port: ${PORT}`);
+    })
+  )
+  .catch(err => console.log(err));
