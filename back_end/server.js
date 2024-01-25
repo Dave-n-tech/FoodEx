bcfdf1da44ec6004431d1834c089ba12962dda37
Miftau <miftaudeenb@gmail.com>
2024-01-25 17:45:03 +0100

    working on the server steadily, installed cors for API integrations

--- back_end/server.js
+++ back_end/server.js
@@ -23,14 +23,14 @@ app.get('/', (req, res) => {
   res.send('Hello world!');
 });
 
-/*production environment
+//production environment
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     app.get('*', (req, res) => {
 res.sendFile(path.resolve(__dirname,'client','build','index.html'));
     });
 }
-*/
+
 
 //MongoDB connection
 const PORT = process.env.PORT || 4000;
